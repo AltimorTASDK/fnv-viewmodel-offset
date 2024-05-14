468d5b284b0b984d7c2727a518528decd5199558
AltimorTASDK <altimormc@gmail.com>
2024-05-14 02:10:30 -0400

    update to more robust solution

--- main.cpp
+++ main.cpp
@@ -1,74 +1,44 @@
-#include <nvse/prefix.h>
-#include <nvse/GameUI.h>
-#include <nvse/PluginAPI.h>
-#include <nvse/NiObjects.h>
 #include <cstddef>
 #include <Windows.h>
 
-class Setting {
-	struct Vtable {
-		virtual ~Vtable() = 0;
-		virtual int vfunc01() = 0;
-	};
+struct IniPrefSetting {
 	void **vtable;
-public:
 	float value;
 	const char *name;
 
-	~Setting()
+	IniPrefSetting(const char *name, float value)
 	{
-		((Vtable&)vtable).~Vtable();
-	}
-};
-
-template<typename T>
-class SettingT;
-
-template<>
-class SettingT<class INIPrefSettingCollection> : public Setting {
-public:
-	SettingT(const char *name, float value)
-	{
-		ThisStdCall(0x4DE370, this, name, value);
+		ThisCall(0x4DE370, this, name, value);
 	}
 };
 
 namespace ini::Viewmodel {
-auto fOffsetX = SettingT<INIPrefSettingCollection>("fOffsetX:Viewmodel",  2.f);
-auto fOffsetY = SettingT<INIPrefSettingCollection>("fOffsetY:Viewmodel",  3.f);
-auto fOffsetZ = SettingT<INIPrefSettingCollection>("fOffsetZ:Viewmodel", -5.f);
+auto fOffsetX = IniPrefSetting("fOffsetX:Viewmodel",  2.f);
+auto fOffsetY = IniPrefSetting("fOffsetY:Viewmodel",  3.f);
+auto fOffsetZ = IniPrefSetting("fOffsetZ:Viewmodel", -5.f);
 }
 
-static void modify_transform()
+static void __fastcall hook_NiAVObject_SetLocalTranslation(
+	NiAVObject *object, int, const NiVector3 &translation)
 {
-	auto *viewmodel_root = *(NiNode**)(0x11E07D8);
-	auto *bip01_rotate = viewmodel_root->m_parent;
-	auto *bip01_translate = bip01_rotate->m_parent;
-	auto *transform = &bip01_translate->dat0034;
-	transform->translate.x = ini::Viewmodel::fOffsetX.value;
-	transform->translate.y = ini::Viewmodel::fOffsetY.value;
-	transform->translate.z = ini::Viewmodel::fOffsetZ.value - 118.F;
+	object->m_transformLocal.translate = translation;
+
+	if (auto *Bip = *(NiNode**)0x11E07D8; object != Bip)
+		return;
+
+	if (InterfaceManager::GetSingleton()->pipBoyMode != 0)
+		return;
+
+	object->m_transformLocal.translate.x += ini::Viewmodel::fOffsetX.value;
+	object->m_transformLocal.translate.y += ini::Viewmodel::fOffsetY.value;
+	object->m_transformLocal.translate.z += ini::Viewmodel::fOffsetZ.value;
 }
 
-__declspec(naked) static void modify_transform_wrapper()
-{
-	__asm {
-		pushad
-		call modify_transform
-		popad
-
-		// Overwritten instructions
-		mov esp, ebp
-		pop ebp
-		retn 8
-	}
-}
-
-static void patch_jmp_rel32(const uintptr_t addr, const void *dest)
+static void patch_call_rel32(const uintptr_t addr, const void *dest)
 {
 	DWORD old_protect;
 	VirtualProtect((void*)addr, 5, PAGE_EXECUTE_READWRITE, &old_protect);
-	*(char*)addr = '\xE9'; // JMP opcode
+	*(char*)addr = '\xE8'; // CALL opcode
 	*(std::byte**)(addr + 1) = (std::byte*)dest - addr - 5;
 	VirtualProtect((void*)addr, 5, old_protect, &old_protect);
 }
@@ -77,13 +47,14 @@ extern "C" __declspec(dllexport) bool NVSEPlugin_Query(const NVSEInterface *nvse
 {
 	info->infoVersion = PluginInfo::kInfoVersion;
 	info->name = "Viewmodel Adjustment";
-	info->version = 1;
+	info->version = 2;
 	return true;
 }
 
 extern "C" __declspec(dllexport) bool NVSEPlugin_Load(NVSEInterface *nvse)
 {
-	// Patch retns
-	patch_jmp_rel32(0x4F059A, modify_transform_wrapper);
+	// Add offset after animating
+	patch_call_rel32(0x4F047D, hook_NiAVObject_SetLocalTranslation);
+	patch_call_rel32(0x4F0551, hook_NiAVObject_SetLocalTranslation);
 	return true;
 }
